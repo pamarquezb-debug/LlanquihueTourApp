12b6d044b08862e21a0e0704f72c657346db54ee
Pablo Marquez <pa.marquezb@duocuc.cl>
2026-07-19 21:05:29 -0400

    Implementa validación de RUT y actualiza documentación

---
+++ src/util/RutInvalidoException.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Excepción personalizada utilizada cuando un RUT
+ * no cumple con las condiciones de validación.
+ *
+ * @author Pablo Márquez Barría
+ * @version 1.0
+ */
+public class RutInvalidoException extends Exception {
+
+    public RutInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
